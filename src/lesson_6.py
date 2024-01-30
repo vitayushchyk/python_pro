d4de5707461a0e2acadff2dbb1498255e36929e9
vitayushchyk <vitay2715@gmail.com>
2024-01-30 21:49:31 +0200

    generators

---
+++ src/lesson_6.py
@@ -0,0 +1,54 @@
+from typing import Generator
+
+
+class Player:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name: str = first_name
+        self.last_name: str = last_name
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+team: list[Player] = [
+    Player("John", "Smith"),
+    Player("Marry", "Smith"),
+    Player("Jack", "Hill"),
+    Player("Nick", "Doe"),
+    Player("John", "Doe"),
+    Player("Marry", "Doe"),
+]
+
+for player in team:
+    print(player)
+
+
+# Output:
+# John Smith
+# Marry Smith
+# Jack Hill
+# Nick Doe
+# John Doe
+
+
+def dedup(collection) -> Generator[str, None, None]:
+    see_all_component = set()
+    for i in collection:
+        if i.first_name not in see_all_component:
+            see_all_component.add(i.first_name)
+            yield str(i)
+
+
+print("___________")
+for player in dedup(team):
+    print(player)
+
+# Expected Output:
+# John Smith
+# Marry Smith
+# Jack Hill
+# Nick Doe
+
+# Explaination:
+# The dedup function should return a generator that yields the unique players in the team.
+# The uniqueness of a player is determined by its first name only
